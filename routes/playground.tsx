a210ed5f3df40accfdae2e6df552b04e60f180b5
robvanvolt <robvanvolt@gmail.com>
2024-05-07 08:06:52 -0700

    addad daisyui

--- routes/playground.tsx
+++ routes/playground.tsx
@@ -3,5 +3,19 @@
 import PlaygroundIsland from "../islands/PlaygroundIsland.tsx";
 
 export default function Playground() {
-  return <PlaygroundIsland />;
+  return (
+    <>
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/daisyui@4.10.5/dist/full.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </head>
+      <html data-theme="bumblebee">
+        <PlaygroundIsland />
+
+      </html>
+    </>
+  );
 }
